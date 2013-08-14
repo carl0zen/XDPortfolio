2edb9d5aee04b363e3fe1610ad5b43b11e58751a
carlosepp <carlosepp@gmail.com>
2013-08-14 08:12:13 -0500

    Ready to push

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,10 @@
 
+jQuery(document).ready(function($) {
+	// Scroll to position
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	});
+});
